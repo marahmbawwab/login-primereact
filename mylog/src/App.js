4952ce4d0f97edc6fd26a3f8e17d1de6d06c77d3
marahmbawwab <marah.bawwab@gmail.com>
2020-09-22 09:31:03 +0300

    "first"

--- mylog/src/App.js
+++ mylog/src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import { InputText } from 'primereact/inputtext';
+import {Password} from 'primereact/password';
 import './App.css';
 
+function form() {
+  return (
+   <div className="p-d-flex p-dir-column">
+   <div className="p-float-label">
+    <label htmlhtmlFor="user">Username</label>
+    <InputText />
+</div>
+<div className="p-password-panel">
+    <label htmlhtmlFor="pass">password</label>
+    <Password />
+</div>
+</div>
+  );
+}
 function App() {
   return (
-    <div className="p-d-block">
-    <p >my name is marah</p>
-    <form className="p-d-flex">
-      <label>hello its me </label>
-      <label>oh what you say </label>
-    </form>
-    </div>
+    
+   <div className="p-d-flex p-dir-row">
+   
+     <p>Welcome To our Website</p>
+
+   <div className="p-field">
+  <label htmlFor="username" className="p-d-block">Username</label>
+  <InputText id="username" aria-describedby="username1-help" className="p-d-block"/>
+  </div>
+
+<div className="p-password-panel">
+    <label htmlhtmlFor="pass">password</label>
+    <Password />
+</div>
+</div>
   );
 }
 
